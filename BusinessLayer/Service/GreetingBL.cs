147e1a0367274e80edccd42b1a9fb134db7a1858
Adityasharma081003 <rs6441419@gmail.com>
2025-03-04 10:43:26 +0530

    [Aditya Sharma] add: adding UC1

---
+++ BusinessLayer/Service/GreetingBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+
+namespace BusinessLayer
+{
+    public class GreetingBL : IGreetingBL
+    {
+        public string GetGreeting()
+        {
+            return "Hello World";
+        }
+    }
+
+
+
+}
+
